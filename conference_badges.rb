e8aa5b13883c4c730708f5ce72a77c25f4d09532
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 00:30:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,5 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(names)
-  names.collect do |speaker| badge_maker(speaker)
-  end
-end
-
-def assign_ro
-  
-def badge_maker(name)
-  "Hello, my name is #{name}."
 end 
 def batch_badge_creator(name)
   arr = []
